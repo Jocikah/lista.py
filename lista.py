f4658b1eb6e6253f03b446383c97f787495e8508
ratkaijozsef <90785017+Jocikah@users.noreply.github.com>
2022-11-15 14:33:03 +0100

    dsa
    asd

---
+++ lista.py
@@ -0,0 +1,43 @@
+#egyenkent probald ki te gyoker
+honapok=["január" ,"február" ,"március" , "április" , "május" , "június"]
+"""print(honapok)
+print(" ," .join(honapok))
+print(len(honapok))
+print(honapok[0])
+print(honapok[len(honapok)-1])
+print(honapok[2:5])
+print(honapok[::-1])
+print(honapok[-2:-6:-1])
+for index in range(len(honapok)):
+    print(index, honapok[index])
+for index, honapok in enumerate(honapok):
+    print(index,honapok)
+ho_masolat = honapok
+print(honapok)
+ho_masolat.sort()
+    print(index,honapok)
+print(ho_masolat)
+print(honapok.index("január"))
+print(honapok.count("március"))
+honapok.append("július")
+print(honapok)
+uj_honapok=["augusztus" , "szeptember" , "október" , "november" , "december"]
+honapok.extend(uj_honapok)
+print(honapok)
+honapok.pop()
+print(honapok)
+honapok.remove("február")
+print(honapok)"""
+import random
+"""i =[]
+for i in range(5):
+    i.append(random.randint(1,99))
+print(i)
+
+i1 =[]
+for i1 in range(1,91,5):
+    i1.append(random.randint(1,99))
+uj_i1=i+i1
+print(uj_i1)
+print(i1*3)"""
+    
